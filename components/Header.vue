776944c41729c6b7ae50d470642cf5baefbaccd6
kskcdsp <ksk.cdsp@gmail.com>
2018-12-29 23:18:37 +0900

    add link other pages

--- components/Header.vue
+++ components/Header.vue
@@ -4,20 +4,26 @@
       <nav class="navbar">
         <div class="container">
           <div class="navbar-brand">
-            <a class="navbar-item has-text-white">
+            <nuxt-link
+              to="/"
+              class="navbar-item has-text-white">
               <font size="5"><b class="holidays-font">Holidays Inc.</b></font>
-            </a>
+            </nuxt-link>
           </div>
           <div
             id="navbarMenuHeroA"
             class="navbar-menu">
             <div class="navbar-end">
-              <a class="navbar-item">
+              <nuxt-link
+                to="/profile"
+                class="navbar-item">
                 <font size="4"><b class="button is-outlined is-white has-text-white header-button-font">Profile</b></font>
-              </a>
-              <a class="navbar-item has-text-white header-button-font">
+              </nuxt-link>
+              <nuxt-link
+                to="/contact"
+                class="navbar-item has-text-white header-button-font">
                 <font size="4"><b class="button is-outlined is-white">Contact</b></font>
-              </a>
+              </nuxt-link>
             </div>
           </div>
         </div>
