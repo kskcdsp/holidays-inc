c1de107c5823e1fd1f38dd60a822c952c63f6a0b
kskcdsp <ksk.cdsp@gmail.com>
2018-12-29 23:12:36 +0900

    change header button

--- components/Header.vue
+++ components/Header.vue
@@ -5,18 +5,18 @@
         <div class="container">
           <div class="navbar-brand">
             <a class="navbar-item has-text-white">
-              <font size="5"><b>Holidays Inc.</b></font>
+              <font size="5"><b class="holidays-font">Holidays Inc.</b></font>
             </a>
           </div>
           <div
             id="navbarMenuHeroA"
             class="navbar-menu">
             <div class="navbar-end">
-              <a class="navbar-item has-text-white">
-                Profile
+              <a class="navbar-item">
+                <font size="4"><b class="button is-outlined is-white has-text-white">Profile</b></font>
               </a>
               <a class="navbar-item has-text-white">
-                Contact
+                <font size="4"><b class="button is-outlined is-white">Contact</b></font>
               </a>
             </div>
           </div>
